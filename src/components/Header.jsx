ad0bd7c14ceb68c2a7f3f0e08cfab0d4ba118e80
Manuel Messon-Roque <manuelmesson@gmail.com>
2019-05-05 01:27:07 -0700

    installed material ui and play with it

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,36 @@
 import React, { Component } from "react";
 import {Link} from 'react-router-dom';
+
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+};
+
 class Header extends Component {
   render = () => {
     return (
-      <header role="banner" className="App-header">
-        <a href="/">Low Income Seattle</a>
+      <header role="banner">
+        {/* <a href="/">Low Income Seattle</a>
         <nav role="navigation" className="header-menu">
         <Link to="/">Home</Link>
           <a href="/contact">Contact</a>
           <Link to='/about'>About</Link>
-        </nav>
+        </nav> */}
+
+<AppBar position="static" color="default">
+        <Toolbar>
+          <Typography variant="h6" color="inherit">
+            Low Income Seattle
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
       </header>
     );
   };
