18a3ee87cef45bdac2c31e926dafb4915acf168a
Manuel Messon-Roque <manuelmesson@gmail.com>
2019-05-10 03:13:40 -0700

    added serverside rendering

--- src/containers/Aside.jsx
+++ src/containers/Aside.jsx
@@ -1,31 +1,31 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-
-import Button from '@material-ui/core/Button';
-import ClickAwayListener from '@material-ui/core/ClickAwayListener';
-import Grow from '@material-ui/core/Grow';
-import Paper from '@material-ui/core/Paper';
-import Popper from '@material-ui/core/Popper';
-import MenuItem from '@material-ui/core/MenuItem';
-import MenuList from '@material-ui/core/MenuList';
-import { withStyles } from '@material-ui/core/styles';
+import Button from "@material-ui/core/Button";
+import ClickAwayListener from "@material-ui/core/ClickAwayListener";
+import Grow from "@material-ui/core/Grow";
+import Paper from "@material-ui/core/Paper";
+import Popper from "@material-ui/core/Popper";
+import MenuItem from "@material-ui/core/MenuItem";
+import MenuList from "@material-ui/core/MenuList";
+import { withStyles } from "@material-ui/core/styles";
 class Aside extends Component {
   render = () => {
     return (
       <aside>
-
-
-<Paper square elevation="2" className="menu">
-                  <ClickAwayListener >
-                    <MenuList className="test">
-                    <Link to="/food-banks"><MenuItem >Food Banks</MenuItem></Link>
-                    <Link to="/free-meals"><MenuItem >Free Meals</MenuItem></Link>
-                      <MenuItem >Public Showers</MenuItem>
-                    </MenuList>
-                  </ClickAwayListener>
-                </Paper>
-
+        <Paper square elevation="1" className="menu">
+          <ClickAwayListener>
+            <MenuList className="test">
+              <Link to="/food-banks">
+                <MenuItem>Food Banks</MenuItem>
+              </Link>
+              <Link to="/free-meals">
+                <MenuItem>Free Meals</MenuItem>
+              </Link>
+              <MenuItem>Public Showers</MenuItem>
+            </MenuList>
+          </ClickAwayListener>
+        </Paper>
       </aside>
     );
   };
