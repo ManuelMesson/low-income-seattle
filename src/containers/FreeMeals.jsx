e6665a54edcc5e0a84e0b4e3c34b66734601847e
Manuel Messon-Roque <manuelmesson@gmail.com>
2019-05-09 23:19:45 -0700

    Created Free Meal Container

---
+++ src/containers/FreeMeals.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class FreeMeals extends Component {
+  componentDidMount = () => {
+      fetch('')
+  };
+  render = () => {
+    return <div />;
+  };
+}
+
+export default FreeMeals;
