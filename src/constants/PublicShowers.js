bd39143c012729d07d1dedd5e0e823b4b294f7fd
Manuel Messon-Roque <manuelmesson@gmail.com>
2019-05-10 17:25:43 -0700

    added public showers data

---
+++ src/constants/PublicShowers.js
@@ -0,0 +1,148 @@
+export const PUBLIC_SHOWERS = [
+    {
+        id: 1,
+        name: 'Angeline\'s Center',
+        phone: '206-436-8650',
+        address: 'YWCA Opportunity Place, 2030 3rd Ave, Seattle, WA',
+        open: 'Daily: 8am-6pm',
+        info: 'Offers laundry and shower facilities, phone and computer access and daily meals for woman.'
+    },
+    {
+        id: 2,
+        name: 'Catholic Community Services',
+        phone: '206-256-0665',
+        address: '1902 2nd Ave, Seattle, WA',
+        open: 'Monday-Friday: 7am-1pm; Saturday and Sunday: 7am-12:30pm',
+        info: 'Showers and Laundry for homeless women 18 and older'
+    }, 
+    {
+        id: 3,
+        name: 'Compass Hygiene Center',
+        phone: '206-474-1600',
+        address: 'Client Services OFfice, 77 S. Washington St, Seattle, WA',
+        open: 'Showers/Restroom: Monday-Friday 7am-2:30pm; Laundry: Monday-Friday 7am-12:45pm',
+        info: 'Offers nursing care and hygiene packets'
+    },
+    {
+        id:4,
+        name: 'Elizabeth Gregory Home',
+        phone: '206-729-0262',
+        address: 'University Lutheran Church, 1604 NE 50th, Seattle, WA',
+        open: 'Monday-Friday: 9am-4:30pm.',
+        info: 'Serves woman 18+. Offers laundry facilities and hygiene kits',
+        
+    },
+    {
+        id: 5,
+        name: 'HYPE - PSKS (Peace for the Streets from Kids from the Streets',
+        phone: '206-726-8500',
+        address: '1609 19th Ave, Seattle, WA',
+        open: 'Monday-Wednesday and Friday: noon-3pm',
+        info: 'Showers, laundry, facilities, meals, clothing & other services for homeless persons ages 18-29'
+    },
+    { 
+        id: 6,
+        name: 'Immanuel Community Services',
+        phone: '206-622-1930',
+        address: '1215 Thomas St, Seattle, WA',
+        open: 'Laundry Monday-Friday 8am-11:30am. Showers Monday-Friday 8am-1:30pm',
+        info: 'Offers laundry and shower facilities, also provides towels and detergent'
+    }, 
+    { 
+        id: 7,
+        name: 'Lazarus Center', 
+        phone: '206-623-7219',
+        address: '2329 Rainer Ave S, Seattle, WA',
+        open: 'Daily: 7am-4pm',
+        info: 'Showers and Laundry. Servers ages 50+. Photo ID required.'
+    }, 
+    {
+        id: 8,
+        name: 'Mary\'s Place-Women\'s Day Center',
+        phone: '206-623-7219',
+        address: '1830 9th Ave, Seattle, WA',
+        open: 'Monday-Friday: 7am-3:30pm; Saturday: 9am-1pm',
+        info: 'Provides showers, restrooms, hygiene items for women and women with children'
+    }, 
+    {
+        id: 9,
+        name: 'New Horizons Ministries', 
+        phone: '206-374-0866',
+        address: '2709 3rd Ave, Seattle, WA',
+        open: 'Monday-Thursday: 8am-10am.',
+        info: 'Drop in services including: showers and laundry for youth 23 and younger.'
+    },
+    {
+        id: 10,
+        name: 'Peter\'s Place',
+        phone: '206-474-1634',
+        address: '901 Rainier Ave. S, Seattle, WA',
+        open: 'Monday-Friday: 9am-12pm',
+        info: 'For homeless adults. Provides separate showers for men and woman',
+    },
+    {
+        id: 11,
+        name: 'Seattle Indian Center',
+        phone: '206-329-8700',
+        address: '1265 S Main St. Suite 105, Seattle, WA',
+        open: 'Monday-Friday: 10am-4:30pm',
+        info: 'open to all, separate area for families with children. Offers laundry and shower facilities. ID Required',
+    }, 
+    {
+        id: 12,
+        name: 'Street Youth Ministries',
+        phone: '206-524-7301, ext. 116',
+        address: '4540 15th Ave NE, Seattle, WA',
+        open: 'Monday, Tuesday and Thursday 7:30pm-9:30pm; Friday 1pm-4pm',
+        info: 'Operates a daily drop-in center for homeless youth and young adults, 13-26; offers showers, laundry, clothing, and more',
+    },
+    {
+        id: 13,
+        name: 'Union Gospel Mission', 
+        phone: '206-622-5177 ext. 5',
+        address: 'Personal Care Center, 318 2nd Ave, Seattle, WA',
+        open: 'Monday-Friday 2pm-4pm',
+        info: 'Showers and restrooms for men only, available first come, first served. No intoxicated clients accepted. Photo ID Required.'
+    },
+    {
+        id: 14,
+        name: 'Urban Rest Stops - Ballard',
+        phone: '206-258-3626',
+        address: '2014 NW 57th St., Seattle, WA', 
+        open: 'Monday-Friday: 6:30am-2:30pm. Closed 10:30am-11:30am',
+        info: 'Serves adults, families, youth and children. Showers lock and are ADA compliant'
+    },
+    {
+        id: 15,
+        name: 'Urban Rest Stops - Downtown',
+        phone: '206-332-0110',
+        address: '1924 9th Ave, Seattle, WA',
+        open: 'Monday-Friday: 5:30am-9:30pm. Closed 11:30am-2pm. On Friday hours vary. Please call for hours. September to May: Saturday and Sunday 8am-3pm. June to September: Saturday and Sunday 8am-5pm',
+        info: 'Provides restrooms, showers and laundry facilities'
+    },
+    {
+        id: 16,
+        name: 'Urban Rest Stops - University District',
+        phone: '206-327-0744',
+        address: 'University Temple UM Church, 1415, NE 43rd St., Seattle, WA',
+        open: 'Tuesday-Thursday 9am-noon and 1pm-6pm; Friday 9am-12:30pm and 1pm-5:30pm',
+        info: 'Segregated area for families. Access through alley'
+    },
+    {
+        id: 17,
+        name: 'YouthCare - Orion Center', 
+        phone: '206-622-5555',
+        address: '1828 Yale Ave, Seattle, WA',
+        open: 'Monday, Wednesday-Friday: 3:30pm-6:30pm; Saturday: 11am-1pm',
+        info: 'Food, showers, laundry facilities, clothing and crisis intervention for youth ages 13-22'
+
+    },
+    {
+        id: 18,
+        name: 'YouthCare - University District',
+        phone: '206-526-2992',
+        address: '4516 15th Ave NE, Seattle, WA',
+        open: 'Monday-Thursday: 1pm-3:30p, or by appointment.',
+        info: 'Showers, laundry facilities and other services for homeless youth ages 13-22'
+    }
+]
